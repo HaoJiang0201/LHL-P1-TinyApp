e2389ba8698457f1d25bf8b1148030c05197c9a5
Polatouche0201 <polatouche@outlook.com>
2019-03-12 19:21:07 +0000

    Tiny App Updated

--- express_server.js
+++ express_server.js
@@ -2,17 +2,23 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+var test = [
+  {name: 'test1'},
+  {name: 'test2'}
+];
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls", (req, res) => {
+
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
