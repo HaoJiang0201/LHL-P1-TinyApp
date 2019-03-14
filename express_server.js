e4b62df9c64ef037857691529ec1ac03b6e88bb5
Polatouche0201 <polatouche@outlook.com>
2019-03-14 21:56:27 +0000

    Tiny App Updated

--- express_server.js
+++ express_server.js
@@ -1,9 +1,19 @@
-var express = require("express");
-var cookieParser = require('cookie-parser')
-var app = express();
-app.use(cookieParser());
-var PORT = 8080; // default port 8080
+/******** Individual Project ********/
+// Name: Tiny App
+// Organization: Lighthouse Lab
+// Course: Web Developer
+// Student: Hao Jiang
+// Time: W2D2 - W2D5
+/************************************/
 
+/******** Extra Module Connect 外部模块连接  ********/
+var express = require("express");
+var cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+
+/******** Global Settings 全局设置、初始化 ********/
+var PORT = 8080; // default port 8080
+// User Info. 用户信息
 const users = {
   "Admin": {
     id: "000000",
@@ -11,12 +21,13 @@ const users = {
     password: "123456"
   },
 }
-
+// Shot-Long URL Database (Object) 存储长短url链接的对象
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+// Random Generate and return a string by given the "string length" to the function
+// 给定字符串长度，随机生成并返回一个字符串
 function randomString(size) {
   let str = "";
   let arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -32,51 +43,31 @@ function randomString(size) {
   return str;
 }
 
-const bodyParser = require("body-parser");
+/******** App Settings & Server Up 应用设置，服务器启动、监听 ********/
+var app = express();
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
+app.listen(PORT, () => {
+  console.log(`Tiny App listening on port ${PORT}!`);
+});
+
+/******** All GET and POST Activities App所有的活动监控 ********/
+// 1. Main Page 主页
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/login");
 });
 
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-  //res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls/");
-});
-
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.userName);
-  res.redirect("/urls");
-});
-
-app.get("/login", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"],
-    login_register: true
-  };
-  res.render("login_register", templateVars);
-});
-
-app.post("/register", (req, res) => {
+// 2. Register， Login & Logout 注册、登录和登出
+app.post("/register", (req, res) => {   // 从
   var usrID = randomString(12);
   var usrEmail = req.body.email;
   var usrPassword = req.body.password;
   var usrPswConfirm = req.body.confirm_password;
   if(usrPassword !== usrPswConfirm) {
     let templateVars = {
-      username: req.cookies["username"],
+      userID: req.cookies["userID"],
       login_register: false,
       password_confirm: false
     };
@@ -86,7 +77,7 @@ app.post("/register", (req, res) => {
     users[usrID].id = usrID;
     users[usrID].email = usrEmail;
     users[usrID].password = usrPassword;
-    res.cookie('username', usrEmail);
+    res.cookie('userID', usrEmail);
     res.redirect("/urls");
     //res.redirect("/register");
   }
@@ -94,16 +85,34 @@ app.post("/register", (req, res) => {
 
 app.get("/register", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    userID: req.cookies["userID"],
     login_register: false,
     password_confirm: true
   };
   res.render("login_register", templateVars);
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('userID', req.body.userName);
+  res.redirect("/urls");
+});
+
+app.get("/login", (req, res) => { //
+  let templateVars = {
+    //username: req.cookies["userID"],
+    login_register: true
+  };
+  res.render("login_register", templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("userID");
+  res.redirect("/urls/");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    userID: req.cookies["userID"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
@@ -121,14 +130,14 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    userID: req.cookies["userID"],
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    userID: req.cookies["userID"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -164,11 +173,19 @@ app.post("/urls/:shortURL/update", (req, res) => {
   res.redirect("/urls");
 });
 
-app.listen(PORT, () => {
-  console.log(`Tiny App listening on port ${PORT}!`);
-});
 
-// Tips:
+
+// X. Test Pages
+// app.get("/hello", (req, res) => {
+//   let templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+//   //res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
 // app.get("/treasure", (req, res) => {
 //   const user = req.cookies.usename && users[req.cookies.username];
 //   if(user) {
